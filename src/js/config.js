277ff05b2759bec4172be8c64e2c6aedc52263d4
Raviphas-R <iamnice2020@gmail.com>
2023-03-11 06:59:02 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd3b0175a-8b56-4e1f-b2cd-0e41a1b8e1d5';
+export const MODAL_CLOSE_SEC = 2.5;
